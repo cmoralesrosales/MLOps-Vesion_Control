693ef793181ec8305a6cc275f55857caf7480ef3
cmoralesrosales <152811408+cmoralesrosales@users.noreply.github.com>
2024-09-21 17:08:14 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="testml"
-LOCATION="eastus"
-WORKSPACE="azureml"
+GROUP="01_MLOps"
+LOCATION="eastus2"
+WORKSPACE="MLOps"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
